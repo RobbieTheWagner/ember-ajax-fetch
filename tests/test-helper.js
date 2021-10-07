a5cccbe05d3ea6248a19adc1b9df3fd16c597b21
Camille TJHOA <camille.tjhoa@concordnow.com>
2021-10-07 06:17:50 +0000

    v3.22.0...v3.24.0
    Using ember-cli-update

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 import QUnit from 'qunit';
@@ -10,5 +12,7 @@ import installVerifyAssertion from 'testdouble-qunit';
 installVerifyAssertion(QUnit, td);
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
