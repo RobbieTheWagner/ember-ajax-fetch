016e42a0d59bf0cf0010474713335c8821723501
AJ McClure <aj.mcclure@expel.io>
2024-03-18 14:01:15 -0500

    Only upgrade babel/traverse transitive dependency

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,15 +5,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
-    autoImport: {
-      webpack: {
-        resolve: {
-          fallback: {
-            module: false,
-          },
-        },
-      },
-    },
   });
 
   /*
