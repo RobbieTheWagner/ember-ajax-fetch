0661df7478977cbbc163582e6eaf9847fcc0ae4a
Robbie Wagner <rwwagner90@gmail.com>
2025-02-17 15:54:46 -0500

    Update ember-try.js

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -6,6 +6,9 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 module.exports = async function () {
   return {
     usePnpm: true,
+    buildManagerOptions() {
+      return ['--ignore-scripts', '--no-frozen-lockfile'];
+    },
     scenarios: [
       {
         name: 'ember-lts-3.28',
