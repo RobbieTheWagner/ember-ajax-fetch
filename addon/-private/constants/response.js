41f50bad194edf3b546d4f33ddfaf77010277317
Saurabh Batra <batrasaurabh@outlook.com>
2025-03-21 00:24:05 +0530

    Add support for compressed types response (#63)
    * Add support for compressed types response
    
    * Return Blob
    
    * Fix pnpm-lock file
    
    ---------
    
    Co-authored-by: Saurabh Batra <sbatra@ciena.com>

---
+++ addon/-private/constants/response.js
@@ -0,0 +1,10 @@
+export const COMPRESSED_TYPES = [
+  'application/zip',
+  'application/x-tar',
+  'application/gzip',
+  'application/x-bzip2',
+  'application/x-7z-compressed',
+  'application/x-rar-compressed',
+  'application/x-xz',
+  'application/vnd.ms-cab-compressed',
+];
